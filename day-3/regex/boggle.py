1dd2440e152041d269159f55ea0399781fd992b3
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-14 13:49:57 +0800

    boggle 99% complete. 1 main bug

--- day-3/regex/boggle.py
+++ day-3/regex/boggle.py
@@ -5,6 +5,10 @@ import string
 board = ['_', '_', '_', '_', '_', '_', '_',
          '_', '_', '_', '_', '_', '_', '_', '_', '_']
 
+coordinates = set()
+
+word_found = False
+
 dice = [
     'AAEEGN',
     'ELRTTY',
@@ -63,50 +67,6 @@ def shake():
         dice.pop(random_index)
 
 
-def check_old(board, str):
-    horizontal_string = ''.join(board)
-    horizontal_string = horizontal_string*3
-
-    regex = f"{str}"
-    match = re.search(regex, horizontal_string)
-
-    if match:
-        print('VALID WORD')
-    else:
-        print('INVALID WORD')
-
-    horizontal_string = horizontal_string[::-1]
-
-    match = re.search(regex, horizontal_string)
-
-    if match:
-        print('VALID WORD')
-    else:
-        print('INVALID WORD')
-
-    vertical_string = ''
-
-    for i in range(4):
-        vertical_string = vertical_string + ''.join(board[i::4])
-
-    vertical_string = vertical_string*3
-
-    match = re.search(regex, vertical_string)
-
-    if match:
-        print('VALID WORD')
-    else:
-        print('INVALID WORD')
-
-    vertical_string = vertical_string[::-1]
-
-    match = re.search(regex, vertical_string)
-
-    if match:
-        print('VALID WORD')
-    else:
-        print('INVALID WORD')
-
 
 shake()
 
@@ -119,7 +79,8 @@ game_box = [
 
 
 def check(word):
-    coordinates = []
+    global word_found
+    global coordinates
     c = word[0]
     if c == 'Q':
         c == 'Qu'
@@ -133,19 +94,40 @@ def check(word):
         for j, char in enumerate(row):
             if c == char:
                 x, y = j, i
-                # print(f"{x}, {y}")
-                # add to list of tuples
-                coordinates.append((x,y))
-                if check_4_real(x, y, word[recursion_index:], coordinates) == True:
-                    return True
-    return False
-
-
-def check_4_real(x, y, word, coordinates*):
-    if len(word) == 1:
-        print('Indeed you have a valid word!')
+                print(f"{x}, {y}")
+                # add to list
+                # coordinates.add((x,y))
+                check_4_real(x, y, word[recursion_index:])
+                # check_4_real(x, y, word)
+                coordinates.clear()
+    if word_found == False:            
+        # coordinates.clear()
+        return False
+    else:
+        # coordinates.clear()
+        word_found = False
         return True
+
+
+def check_4_real(x, y, word):
+    global word_found
+    global coordinates
+    
+    print(coordinates)
+    #base case - termination
+    # breakpoint()
+    if len(word) == 1:
+        if (x,y) not in coordinates:
+            # print("HOORAY")
+            word_found = True
+            return True
+        elif (x,y) in coordinates:
+            return False
+        else:
+            return False
+    coordinates.add((x,y))
     c = word[0]
+    # c = word[1]
     if c == 'Q':
         c == 'Qu'
         recursion_index = 2
@@ -154,50 +136,44 @@ def check_4_real(x, y, word, coordinates*):
     # check left
     if x>0:
         if game_box[y][x-1] == c and (x-1,y) not in coordinates:
-            coordinates.append((x-1,y))
+            # coordinates.add((x-1,y))
             check_4_real(x-1, y, word[recursion_index:])
     # check right
     if x < (len(game_box[y])-1):
         if game_box[y][x+1] == c and (x+1,y) not in coordinates:
-            coordinates.append((x+1,y))
+            # coordinates.add((x+1,y))
             check_4_real(x+1, y, word[recursion_index:])
     # check top
     if y>0:
         if game_box[y-1][x] == c and (x,y-1) not in coordinates:
-            coordinates.append((x,y-1))
+            # coordinates.add((x,y-1))
             check_4_real(x, y-1, word[recursion_index:])
     # check bottom
     if y<3:
         if game_box[y+1][x] == c and (x,y+1) not in coordinates:
-            coordinates.append((x,y+1))
+            # coordinates.add((x,y+1))
             check_4_real(x, y+1, word[recursion_index:])
     # check top left
     if x>0 and y>0:
         if game_box[y-1][x-1] == c and (x-1,y-1) not in coordinates:
-            coordinates.append((x-1,y-1))
+            # coordinates.add((x-1,y-1))
             check_4_real(x-1,y-1,word[recursion_index:])
     # check bottom left
     if x>0 and y<3:
         if game_box[y+1][x-1] == c and (x-1,y+1) not in coordinates:
-            coordinates.append((x-1,y+1))
+            # coordinates.add((x-1,y+1))
             check_4_real(x-1,y+1,word[recursion_index:])
     # check top right
-    if x< (len(game_box[y]) - 1) and y>0:
+    if x < (len(game_box[y]) - 1) and y>0:
         if game_box[y-1][x+1] == c and (x+1,y-1) not in coordinates:
-            coordinates.append((x+1,y-1))
+            # coordinates.add((x+1,y-1))
             check_4_real(x+1,y-1,word[recursion_index:])
     # check bottom right
     if x < (len(game_box[y])-1) and y < 3:
         if game_box[y+1][x+1] == c and (x+1,y+1) not in coordinates:
-            coordinates.append((x+1,y+1))
+            # coordinates.add((x+1,y+1))
             check_4_real(x+1,y+1,word[recursion_index:])
 
     return False
-
-#recursion ['WORD']
-# ['ORD']
-# ['RD']
-# ['D']
-# if len word == 1, return True, else return false
 
 print_board()
