5fc92051aeaf8633aed7e6c0c2a7fed78f35a3b3
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-22 13:59:29 +0800

    added index.html template

--- flask/flask-practice/server.py
+++ flask/flask-practice/server.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/") # Revisit decorators if you unclear of this syntax
 def index():
-    return '<h1>Why so easy</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<username>')
 def show(username):
-    return f"Hi {username[0:3]}"
+    return f"Hi {username}"
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run(debug=True)
