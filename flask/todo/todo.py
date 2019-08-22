08722d5ecb11d99c83583ebfcd7ae12e2038eef1
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-22 11:50:01 +0800

    flask todo app v0 complete

--- flask/todo/todo.py
+++ flask/todo/todo.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import peewee as pw
+
+db = pw.PostgresqlDatabase("todo_list")
+
+class Todo(pw.Model):
+    task = pw.CharField(null=False)
+    
+    class Meta:
+        database = db
+
+db.connect()
+db.create_tables([Todo])
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
-    return "<h1>Hello<h1>"
+    todos =Todo.select()
+    return render_template("index.html", todos=todos)
 
-app.run()
+@app.route('/', methods=["POST"])
+def add_todo():
+    print()
+    print(request.form)
+    print()
+    task = request.form["task"]
+    Todo.create(task=task)
+    return redirect('/')
+
+app.run(debug=True)
