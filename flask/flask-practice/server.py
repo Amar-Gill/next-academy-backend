2caed11ac27eaa0e5368de1789537e255f0c988b
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-22 15:25:49 +0800

    added master template. signin/signout is WIP

--- flask/flask-practice/server.py
+++ flask/flask-practice/server.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 @app.route("/") # Revisit decorators if you unclear of this syntax
 def index():
     signed_in = False
-    return render_template('index.html', signed_in = signed_in)
+    return render_template('index.html', signed_in=signed_in)
 
 @app.route('/user/<username>')
-def show(username):
+def show_username(username):
     return f"Hi {username}"
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run(debug=True)
