6c0dfa05b1d4533347d3ae85aaffb54403c31849
Amar Singh Gill <amar@Amars-MacBook-Air.local>
2019-08-22 15:58:06 +0800

    added link on homepage to contact page

--- flask/flask-practice/server.py
+++ flask/flask-practice/server.py
@@ -12,7 +12,8 @@ def show_username(username):
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    signed_in = False
+    return render_template('contact.html', signed_in=signed_in)
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run(debug=True)
